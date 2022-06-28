83a07a26e548e5ec9ccbff467e9b3a99c746da04
CodeExplainedRepo <41912954+CodeExplainedRepo@users.noreply.github.com>
2022-06-28 12:47:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,109 +1,141 @@
-// elements
-const todoform = document.getElementById('todoform');
-const newtodo = document.getElementById('newtodo');
-const todosList = document.getElementById('todos-list');
+// SELECT ELEMENTS
+const form = document.getElementById('todoform');
+const todoInput = document.getElementById('newtodo');
+const todosListEl = document.getElementById('todos-list');
+const notificationEl = document.querySelector('.notification');
 
-// vars
-let todos = [];
-let tempTodoId = '';
+// VARS
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+let EditTodoId = -1;
 
-// load local storage if there is any
-if (localStorage.getItem('todos')) {
-  todos = JSON.parse(localStorage.getItem('todos'));
-  renderTodos();
-}
+// 1st render
+renderTodos();
 
-// listen to form submission
-todoform.addEventListener('submit', function (e) {
-  e.preventDefault();
+// FORM SUBMIT
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-  // save to do
   saveTodo();
   renderTodos();
   localStorage.setItem('todos', JSON.stringify(todos));
 });
 
-// add even listener to todos
-todosList.addEventListener('click', (e) => {
-  const target = e.target;
-
-  const todo = target.parentNode;
-  if (todo.className !== 'todo') return;
-
-  const todoId = Number(todo.id);
-  const action = target.dataset.action;
-
-  action === 'check' &&
-    (todos = todos.map((todo, index) => ({
-      ...todo,
-      checked: index === todoId ? !todo.checked : todo.checked,
-    })));
-
-  action === 'delete' && (todos = todos.filter((todo, index) => index !== todoId)) && (tempTodoId = '');
-
-  action === 'edit' && (newtodo.value = todos[todoId].value) && (tempTodoId = todoId);
-
-  renderTodos();
-  localStorage.setItem('todos', JSON.stringify(todos));
-});
-// save new todo
+// SAVE TODO
 function saveTodo() {
-  if (!newtodo.value) {
-    showNotification("todo's empty!");
-  } else if (todos.findIndex((todo) => todo.value.toUpperCase() === newtodo.value.toUpperCase()) >= 0) {
-    showNotification('Duplicate todo');
+  const todoValue = todoInput.value;
+
+  // check if the todo is empty
+  const isEmpty = todoValue === '';
+
+  // check for duplicate todos
+  const isDuplicate = todos.some((todo) => todo.value.toUpperCase() === todoValue.toUpperCase());
+
+  if (isEmpty) {
+    showNotification("Todo's input is empty");
+  } else if (isDuplicate) {
+    showNotification('Todo already exists!');
   } else {
-    if (tempTodoId !== '') {
+    if (EditTodoId >= 0) {
       todos = todos.map((todo, index) => ({
         ...todo,
-        value: index === tempTodoId ? newtodo.value : todo.value,
+        value: index === EditTodoId ? todoValue : todo.value,
       }));
-      tempTodoId = '';
+      EditTodoId = -1;
     } else {
-      todos.unshift({
-        value: newtodo.value,
+      todos.push({
+        value: todoValue,
         checked: false,
         color: '#' + Math.floor(Math.random() * 16777215).toString(16),
       });
     }
-    newtodo.value = '';
+
+    todoInput.value = '';
   }
 }
 
-// render todos
+// RENDER TODOS
 function renderTodos() {
-  if (!todos.length) {
-    todosList.innerHTML = '<center>Nothing to do</center>';
+  if (todos.length === 0) {
+    todosListEl.innerHTML = '<center>Nothing to do!</center>';
     return;
   }
 
-  todosList.innerHTML = '';
+  // CLEAR ELEMENT BEFORE A RE-RENDER
+  todosListEl.innerHTML = '';
+
+  // RENDER TODOS
   todos.forEach((todo, index) => {
-    todosList.innerHTML += `<div class="todo" id='${index}'>
-                                <i class="bi ${todo.checked ? 'bi-check-circle-fill' : 'bi-circle'}"
-                                style="color : ${todo.color}"
-                                data-action="check"></i>
-                                
-                                <p class="${todo.checked ? 'checked' : ''}" data-action="check">
-                                    ${todo.value}
-                                </p>
-
-                                <i class="bi bi-pencil-square" data-action="edit"></i>
-
-                                <i class="bi bi-trash" data-action="delete"></i>
-                            </div>`;
+    todosListEl.innerHTML += `
+    <div class="todo" id=${index}>
+      <i 
+        class="bi ${todo.checked ? 'bi-check-circle-fill' : 'bi-circle'}"
+        style="color : ${todo.color}"
+        data-action="check"
+      ></i>
+      <p class="${todo.checked ? 'checked' : ''}" data-action="check">${todo.value}</p>
+      <i class="bi bi-pencil-square" data-action="edit"></i>
+      <i class="bi bi-trash" data-action="delete"></i>
+    </div>
+    `;
   });
 }
 
-function showNotification(message) {
-  const notifEl = document.querySelector('.notification');
+// CLICK EVENT LISTENER FOR ALL THE TODOS
+todosListEl.addEventListener('click', (event) => {
+  const target = event.target;
+  const parentElement = target.parentNode;
 
-  // show message
-  notifEl.innerHTML = message;
+  if (parentElement.className !== 'todo') return;
 
-  notifEl.classList.add('notif-enter');
+  // t o d o id
+  const todo = parentElement;
+  const todoId = Number(todo.id);
 
+  // target action
+  const action = target.dataset.action;
+
+  action === 'check' && checkTodo(todoId);
+  action === 'edit' && editTodo(todoId);
+  action === 'delete' && deleteTodo(todoId);
+});
+
+// CHECK A TODO
+function checkTodo(todoId) {
+  todos = todos.map((todo, index) => ({
+    ...todo,
+    checked: index === todoId ? !todo.checked : todo.checked,
+  }));
+
+  renderTodos();
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+// EDIT A TODO
+function editTodo(todoId) {
+  todoInput.value = todos[todoId].value;
+  EditTodoId = todoId;
+}
+
+// DELETE TODO
+function deleteTodo(todoId) {
+  todos = todos.filter((todo, index) => index !== todoId);
+  EditTodoId = -1;
+
+  // re-render
+  renderTodos();
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+// SHOW A NOTIFICATION
+function showNotification(msg) {
+  // change the message
+  notificationEl.innerHTML = msg;
+
+  // notification enter
+  notificationEl.classList.add('notif-enter');
+
+  // notification leave
   setTimeout(() => {
-    notifEl.classList.remove('notif-enter');
+    notificationEl.classList.remove('notif-enter');
   }, 2000);
 }
